00706bb1c80540a5f12650b8f0f9ca19da92155f
Cameron H <cameronheadlee6@gmail.com>
2021-09-16 13:14:06 -0400

    adding typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,36 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID!
+    firstName: String
+    lastName: String
     username: String!
     email: String
+    orders: [Order]
+  }
+
+  type Comment { 
+    _id: ID
+    commentText: String
+    
+  }
+
+  type Product { 
+    _id: ID
+    name: String
+    description: String
+    image: String
+    price: Float
+    category: Category 
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
+
+  type Checkout{
+    session: ID
   }
 
   type Auth {
@@ -14,12 +42,20 @@ const typeDefs = gql`
 
 
   type Query {
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
     me: User
+    order(_id: ID!): order
+    checkout(products: [ID]!): Checkout
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
+    addUser(firstName: String! lastName: String! username: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+
    
   }
 `;
